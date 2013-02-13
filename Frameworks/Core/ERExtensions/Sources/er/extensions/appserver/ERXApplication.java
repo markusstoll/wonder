88c56a69d01f561ffc2959b3f9bf9e35cb34ea04
Markus Stoll <markus.stoll@junidas.de>
2013-02-13 22:20:00 +0100

    bugfix gzip compression and static resource loading (HTTP version was dropped)

--- Frameworks/Core/ERExtensions/Sources/er/extensions/appserver/ERXApplication.java
+++ Frameworks/Core/ERExtensions/Sources/er/extensions/appserver/ERXApplication.java
@@ -2130,30 +2130,33 @@ public abstract class ERXApplication extends ERXAjaxApplication implements ERXGr
 				if ((acceptEncoding != null) && (acceptEncoding.toLowerCase().indexOf("gzip") != -1)) {
 					long start = System.currentTimeMillis();
 					long inputBytesLength;
+					int compressedLength = 0;
+					
 					InputStream contentInputStream = response.contentInputStream();
 					byte[] compressedData;
 					if (contentInputStream != null) {
 						inputBytesLength = response.contentInputStreamLength();
 						NSData compressedNSData = ERXCompressionUtilities.gzipInputStreamAsNSData(contentInputStream, (int)inputBytesLength);
-						//compressedData = compressedNSData._bytesNoCopy();
-						compressedData = compressedNSData.bytes();
+						compressedData = compressedNSData._bytesNoCopy();
 						response.setContentStream(null, 0, 0L);
+						compressedLength = compressedNSData.length();
 					}
 					else {
 						NSData input = response.content();
 						inputBytesLength = input.length();
 						compressedData = (inputBytesLength > 0) ? ERXCompressionUtilities.gzipByteArray(input._bytesNoCopy()) : null;
+						compressedLength = compressedData.length;
 					}
 					if ( inputBytesLength > 0 ) {
 						if (compressedData == null) {
 							// something went wrong
 						}
 						else {
-							response.setContent(new NSData(compressedData, new NSRange(0, compressedData.length), true));
-							response.setHeader(String.valueOf(compressedData.length), "content-length");
+							response.setContent(new NSData(compressedData, new NSRange(0, compressedLength), true));
+							response.setHeader(String.valueOf(compressedLength), "content-length");
 							response.setHeader("gzip", "content-encoding");
 							if (log.isDebugEnabled()) {
-								log.debug("before: " + inputBytesLength + ", after " + compressedData.length + ", time: " + (System.currentTimeMillis() - start));
+								log.debug("before: " + inputBytesLength + ", after " + compressedLength + ", time: " + (System.currentTimeMillis() - start));
 							}
 						}
 					}
